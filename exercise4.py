5cd33618106759912e00450e01783d2906744b32
chandra sekhar reddy <srinivasabasireddy06@gmail.com>
2026-02-18 20:03:40 +0530

    update

--- exercise4.py
+++ exercise4.py
@@ -164,4 +164,84 @@ Input: 123, Output: 321 Input: 120, Output: 21 '''
 # reverse(num)
 
 
-'''  '''
+''' Write a function that takes as an argument a parameter of type Point and returns True if it
+lies within the square connecting the points (0, 0), (1, 0), (0, 1), (1, 1); and False otherwise. '''
+# def is_inside_square(point):
+#     x, y = point
+#     if 0 <= x <= 1 and 0 <= y <= 1:
+#         return True
+#     else:
+#         return False
+# p1 = x, y = float(input("enter x - coordinate of the point :")), float(input("enter y - coordinate of the point :"))
+# print(f"{is_inside_square(p1)}")
+
+
+''' Write a Python function called quadratic roots that takes three arguments (a, b, c) represent-
+ing the coefficients of a quadratic equation ax2 + bx + c = 0. The function should return the
+positive real root if one exists; otherwise, return None. '''
+# def quadratic_roots(a, b, c):
+#     d = 0
+#     d = (b** 2)- (4*a*c)
+#     if d >= 0:
+#         root1,root2 = (-b + d**1/2)/2*a , (-b - d**1/2)/2*a
+#         if root1 > 0:
+#             return root1
+#         elif root2 > 0:
+#             return root2
+#         else:
+#             return None
+#     else:
+#         return None
+# a,b,c = float(input("enter co-efficient of x^2 :")), float(input("enter co-efficient of x :")), float(input("enter constant value :"))
+# print(f"{quadratic_roots(a,b,c)}")
+
+
+''' Write a function cubesum that accepts a 3-digit integer and returns the sum of the cubes of
+individual digits of that number. Use this function in another function isArmstrong to check
+if its 3-digit parameter is an Armstrong number. Example: 371 is an Armstrong number since
+3^3 + 7^3 + 1^3 = 371. '''
+# def amstrong_number_check(org_number):
+#     number = org_number
+#     total_sum = 0
+#     while number > 0:
+#         rem = number % 10
+#         total_sum += rem**3
+#         number //= 10
+#     if total_sum == org_number:
+#         return True
+#     else:
+#         return False
+# num = int(input("enter a 3-digit integer number to check it is a amstrong number or not :"))
+# print(f"{amstrong_number_check(num)}")
+
+
+''' Write a function prodDigits that takes a 5-digit number as its parameter and returns the
+product of the digits of the number. '''
+# def prodDigits(num):
+#     org_num = num
+#     pro = 1
+#     count = 0
+#     while num > 0:
+#         rem = num % 10
+#         pro *= rem
+#         num //= 10
+#         count+=1
+#     if count == 5:
+#         print(f"products of digits in {org_num} = {pro}")
+#     else:
+#         print("please enter a 5-digit number.")
+# num = int(input("enter a 5-digit number :"))
+# prodDigits(num)
+
+
+''' Write a function that returns the non-negative square root of a number rounded to the nearest
+integer. '''
+# def round_square_root(num):
+#     if num < 0:
+#         print("enter a non-neagtive number to find square root.")
+#     else:
+#         sqrt_num = num**0.5
+#         sqrt_num = sqrt_num//1
+#         print(f"square root of {num} = {sqrt_num}")
+# num = int(input("enter a non-negative number to find square root :"))
+# round_square_root(num)
